a242f59bf4c163cc81c29955f0ec9daaacf88d55
Sehal Sein <seinsehal@gmail.com>
2025-02-23 11:19:08 -0500

    fix: resume redirect

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function Tag({ children }: PropsWithChildren) {
   return (
-    <span className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white font-medium">
+    <span className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white font-medium print:p-0 print:text-black">
       {children}
     </span>
   );
